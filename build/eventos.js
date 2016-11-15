4dbba8a9a5da825a6e0883e56df310e48f65f65c
Henrique <rick@gartic.com>
2016-11-15 01:22:14 -0200

    JSDoc

--- build/eventos.js
+++ build/eventos.js
@@ -1,21 +1,42 @@
 'use strict';
+/**
+ * Classe para o tratamento de eventos próprios no estilo node (EventEmitter)
+ */
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Eventos = function () {
+	/**
+  * Construtor da classe, inicializando a lista de eventos.
+  */
 	function Eventos() {
 		_classCallCheck(this, Eventos);
 
 		this._lista = [];
 	}
 
+	/**
+  * Criando um evento
+  *
+  * @param {string} evento Nome/indice do evento a ser adicionado
+  * @param {function} callback Função a ser chamada assim que o evento for disparado
+  */
+
+
 	_createClass(Eventos, [{
 		key: 'on',
 		value: function on(evento, callback) {
 			this._lista[evento] = callback;
 		}
+
+		/**
+   * Emitindo um evento
+   *
+   * @param {string} evento Nome/indice do evento a ser disparado
+   */
+
 	}, {
 		key: 'emit',
 		value: function emit(evento) {
--- src/eventos.js
+++ src/eventos.js
@@ -1,14 +1,30 @@
 'use strict';
-
+/**
+ * Classe para o tratamento de eventos próprios no estilo node (EventEmitter)
+ */
 class Eventos {
+	/**
+	 * Construtor da classe, inicializando a lista de eventos.
+	 */
 	constructor() {
 		this._lista = [];
 	}
 
+	/**
+	 * Criando um evento
+	 *
+	 * @param {string} evento Nome/indice do evento a ser adicionado
+	 * @param {function} callback Função a ser chamada assim que o evento for disparado
+	 */
 	on(evento, callback) {
 		this._lista[evento] = callback;
 	}
 
+	/**
+	 * Emitindo um evento
+	 *
+	 * @param {string} evento Nome/indice do evento a ser disparado
+	 */
 	emit(evento, ...args) {
 		if(this._lista[evento])
 			this._lista[evento](...args);
